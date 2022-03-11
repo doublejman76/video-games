0ad832f11a4a779f45297f113b31e0c346dd9ba0
doublejman76 <doublejman76@hotmail.com>
2022-03-10 20:26:03 -0600

    Initial commit

---
+++ app/models/video_game.rb
@@ -0,0 +1,5 @@
+class VideoGame < ApplicationRecord
+    validates :title, presence:true, length: {minimum: 2, maximum: 100}
+    validates :description, presence:true, length: {minimum: 6, maximum: 200}
+    validates :genre, presence:true, length: {minimum: 2, maximum: 100}
+end
---
+++ db/migrate/20220311020639_create_video_games.rb
@@ -0,0 +1,11 @@
+class CreateVideoGames < ActiveRecord::Migration[6.1]
+  def change
+    create_table :video_games do |t|
+      t.string :name
+      t.text :description
+      t.string :genre
+
+      t.timestamps
+    end
+  end
+end
