7e7526c7ced4fb9a814e9f20f1b2ed643e8cf307
doublejman76 <doublejman76@hotmail.com>
2022-03-14 20:25:42 -0500

    CRUD functions

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -1,9 +1,26 @@
 class VideoGamesController < ApplicationController
+
   def show
-    
+      @video_games = VideoGame.find(params[:id])
   end
 
   def index
-
+      @video_games = VideoGame.all
   end
+
+  def new
+      
+  end
+
+  def create
+    @video_games = VideoGame.new(name: params[:name], description: params[:description])
+    @video_games.save
+    redirect_to @video_games
+  if @video_games.save
+      flash[:notice] = "Video Game was created successfully"
+      redirect_to @video_games
+  else
+    render 'new'
+  end
+ end
 end
