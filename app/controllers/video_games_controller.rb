4539b999287926d0d7bc95400481ff1187571988
doublejman76 <doublejman76@hotmail.com>
2022-03-19 20:35:04 -0500

    Video Game app ready for deployment

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -1,7 +1,7 @@
 class VideoGamesController < ApplicationController
+  before_action :set_video_game, only: [:show, :edit, :destroy, :update]
 
   def show
-      @video_game = VideoGame.find(params[:id])
   end
 
   def index
@@ -13,16 +13,41 @@ class VideoGamesController < ApplicationController
   end
 
   def edit
-      @video_game = VideoGame.find(params[:id])
   end
 
   def create
-    @video_game = VideoGame.new(name: params[:name], description: params[:description], genre: params[:genre])
-    @video_game.save
-  if  flash[:notice] = "Video Game was created successfully"
-      redirect_to @video_games
-  else
-    render 'new'
+    @video_game = VideoGame.new(video_games_params)
+    if @video_game.save
+      flash[:notice] = "Video Game was created successfully"
+      redirect_to @video_game
+    else
+      render'new'
+    end
   end
- end
+
+  def update
+    if @video_game.update(video_games_params)
+      flash[:message] = "Update successful!"
+      redirect_to @video_game
+    else
+      render 'edit'
+    end    
+  end
+
+  def destroy
+    flash[:message] = "#{@video_game.name} was successfully deleted."
+    @video_game.destroy
+    redirect_to video_games_path
+  end
+
+  private
+
+  def set_video_game
+    @video_game = VideoGame.find(params[:id])
+  end
+
+  def video_games_params
+    params.require(:video_game).permit(:name, :description, :genre)
+  end
+
 end
